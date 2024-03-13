f26d2e03476ad90cd8822822b1f6d3a13776125e
1829122 <100738276+1829122@users.noreply.github.com>
2024-03-13 20:28:15 +0900

    Create SmokestackDataApplication.java

---
+++ SmokeStackData/SmokestackDataApplication.java
@@ -0,0 +1,16 @@
+package Smokestack.SmokestackData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SmokestackDataApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(SmokestackDataApplication.class, args);
+		DataParserService dataParserService = context.getBean(DataParserService.class);
+		dataParserService.parseData();
+		dataParserService.parseAndSaveData();
+	}
+}
