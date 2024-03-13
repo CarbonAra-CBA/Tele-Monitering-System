89f2a51c1a2eb357c5344a5008f8741f998e9577
1829122 <100738276+1829122@users.noreply.github.com>
2024-03-13 22:53:27 +0900

    Update SSData.java

--- SmokeStackData/SSData.java
+++ SmokeStackData/SSData.java
@@ -21,19 +21,21 @@ public class SSData {
 
     private String Stack_code;
 
-    private String Tsp; // 먼지
+    private double Tsp; // 먼지
 
-    private String Nox; // 질소산화물
+    private double Nox; // 질소산화물
 
-    private String Sox; //황산화물
+    private double Sox; //황산화물
 
-    private String Hcl; // 염화수소
+    private double Hcl; // 염화수소
 
-    private String Hf; // 불화수소
+    private double Hf; // 불화수소
 
-    private String Nh3; //암모니아
+    private double Nh3; //암모니아
 
-    private String Co; //일산화탄소
+    private double Co; //일산화탄소
+
+    private double Sum;
 
 
 }
