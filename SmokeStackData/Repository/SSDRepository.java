7fd4f8786a1511587d91ca05ee323c67983f038c
1829122 <100738276+1829122@users.noreply.github.com>
2024-03-18 02:54:06 +0900

    Update SSDRepository.java

--- SmokeStackData/Repository/SSDRepository.java
+++ SmokeStackData/Repository/SSDRepository.java
@@ -1,9 +1,18 @@
-package Smokestack.SmokestackData;
+package Smokestack.SmokestackData.Repository;
 
+
+import Smokestack.SmokestackData.Table.SSData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SSDRepository extends JpaRepository<SSData, Integer> {
-    
+    List<SSData> findAll();
+    List<SSData> findByfactmanagenm(String keyword);
+
+
+
+
 }
