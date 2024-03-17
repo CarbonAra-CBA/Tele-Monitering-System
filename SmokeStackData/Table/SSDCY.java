a1b79c0a84dd35b2bb0e55766fd2352436295994
1829122 <100738276+1829122@users.noreply.github.com>
2024-03-17 15:34:06 +0900

    Update SSDCY.java

--- SmokeStackData/Table/SSDCY.java
+++ SmokeStackData/Table/SSDCY.java
@@ -1,14 +1,17 @@
-package Smokestack.SmokestackData;
+package Smokestack.SmokestackData.Table;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
-// 각 기업별 3년치 상세내역을 하려고 했으나 너무 많을거 같아서 Top10만 할 생각
+
 @Entity
 @Getter @Setter
 public class SSDCY {
     @Id
-    private Long id;
+    private String name;
+    private String address;
+    private double latitude;
+    private double longitude;
 
 }
