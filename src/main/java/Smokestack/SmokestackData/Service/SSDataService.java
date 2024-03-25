edd4a762ac87c8f6effe2cad204da94aafaaafe9
Jelly1500 <58739539+Jelly1500@users.noreply.github.com>
2024-03-25 21:47:12 +0900

    DataParserService.java, SSDataService.java 주석 추가
    1. parseData() 함수의 기능에 대한 간략한 주석 추가
    2. parseAndSaveData() 함수의 기능에 대한 간략한 주석 추가
    3. SSDataService.java에서 findByfactmanagenm() 함수의 기능을 간략하게 명시

--- src/main/java/Smokestack/SmokestackData/Service/SSDataService.java
+++ src/main/java/Smokestack/SmokestackData/Service/SSDataService.java
@@ -17,6 +17,7 @@ public class SSDataService {
     SSDRepository ssdRepository;
     @Transactional
     public List<SSData> serach(String keyword){
+        /*공장을 관리하는 회사의 이름을 기준으로 그 회사의 관리 하에 있는 공장들을 가져오기*/
         List<SSData> ssdlist = ssdRepository.findByfactmanagenm(keyword);
         return ssdlist;
     }
