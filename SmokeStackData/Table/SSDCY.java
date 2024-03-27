ac8b86c6f225451dd06d6b4c656398923edf55dc
1829122 <100738276+1829122@users.noreply.github.com>
2024-03-28 00:05:40 +0900

    Update SSDCY.java

--- SmokeStackData/Table/SSDCY.java
+++ SmokeStackData/Table/SSDCY.java
@@ -7,11 +7,26 @@ import lombok.Setter;
 
 @Entity
 @Getter @Setter
-public class SSDCY {
+public class SSDCY { // 회사 주소
     @Id
     private String name;
+
     private String address;
-    private double latitude;
-    private double longitude;
+
+    private double tsp; // 먼지
+
+    private double nox; // 질소산화물
+
+    private double sox; //황산화물
+
+    private double hcl; // 염화수소
+
+    private double hf; // 불화수소
+
+    private double nh3; //암모니아
+
+    private double co; //일산화탄소
+
+    private double sum;
 
 }
